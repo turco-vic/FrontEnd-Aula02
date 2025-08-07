8e3db659a4f2a75e86edb5cede2f275e695d3cf6
Enzo Alves Turcovic <enzoturcovic@gmail.com>
2025-08-07 11:49:20 -0300

    Update

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,17 @@
 import styles from "./Header.module.css";
+import Image from "next/image";
 
-export default function Header({title, subtitle, totalProducts}) {
+export default function Header({ title, subtitle, totalProducts }) {
     return (
         <div className={styles.container}>
-            <h1 className={styles.title}>{title}</h1>
-            <h3 className={styles.subtitle}>{subtitle}</h3>
-            <p className={styles.description}>{totalProducts}</p>
+            <div className={styles.logo}>
+                <Image src="/images/logoturco.png" alt="Logo" width={100} height={100} priority />
+            </div>
+            <div className={styles.text}>
+                <h1 className={styles.title}>{title}</h1>
+                <h3 className={styles.subtitle}>{subtitle}</h3>
+                <p className={styles.description}>{totalProducts}</p>
+            </div>
         </div>
     );
 }
